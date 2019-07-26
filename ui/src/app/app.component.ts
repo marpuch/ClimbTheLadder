f41dfc799ee9b81e73c3dbc98889d290fe81fa59
Marek Puchalski <marek.puchalski@capgemini.com>
2019-07-26 23:35:51 +0200

    Table + query service present.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {QueryService} from "./services/query.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+  title = 'Climb The Ladder';
+  public listItems;
+
+  constructor(private queryService : QueryService) {}
+
+  ngOnInit(): void {
+    this.getListItems();
+  }
+
+  getListItems() {
+    this.queryService.get().subscribe(
+        data => { this.listItems = data },
+        err => { console.error(err) },
+        () => console.log("Items loaded")
+    )
+  }
 }
---
+++ ui/src/app/services/query.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers : new HttpHeaders({'Content-Type' : 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QueryService {
+
+  constructor(private http:HttpClient) { }
+
+  get() {
+    return this.http.get('/query');
+  }
+}
